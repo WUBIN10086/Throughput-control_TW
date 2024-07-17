34b506ba37e9c3ff7adff18eb3120c54abe341e5
HoChengYun <h45249210@gmail.com>
2024-07-17 10:55:40 +0900

    註解

--- traffic_shaping_code/traffic_control.sh
+++ traffic_shaping_code/traffic_control.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #This scripts control incomming and outcomming bandwight in a linux router box
 
-dir=/home/pi/fairness_AP1
-unit=mbit
+dir=/home/pi/fairness_AP1  #指定配置文件的目錄。
+unit=mbit  #設定帶寬單位為Mbit。
 # Interface connect to out lan
-int1="wlan0"
+int1="wlan0"  #設定連接到LAN的網路介面
 # Interface virtual for incomming traffic
-tin1="ifb0"
+tin1="ifb0"  #設定虛擬介面，用於進入流量
 
+#使用wc -l命令從文件set_target_throughput.tmp中讀取總數據速率。
 totalDatarate=$(< $dir/set_target_throughput.tmp wc -l)
 
+#使用awk從文件中讀取每個IP和對應的數據速率，並存儲在陣列IP和datarate中。
 w=1
 while [ $w -le $totalDatarate ]
 do
@@ -18,6 +20,7 @@ do
   w=$(( w+1 ))
 done
 
+#累加所有的數據速率，計算出總數據速率total_th
 w=1
 total_th=0
 while [ $w -le $totalDatarate ]
@@ -26,23 +29,26 @@ do
   w=$(( w+1 ))
 done
 
+#使用modprobe載入ifb模組，並將虛擬介面tin1啟用
 modprobe ifb numifbs=1
 sudo ip link set dev $tin1 up
 
 ## Limit incomming traffic ( to localhost)
-# Clean interface
+# Clean interface(清除之前的qdisc設定)
 sudo tc qdisc del dev $int1 handle ffff: ingress
-#modprobe -r ifb
+#modprobe -r ifb(設定新的qdisc來限制進入流量)
 sudo tc qdisc del root dev $tin1
 sudo tc qdisc add dev $int1 handle ffff: ingress
-# Redirect  ingress wlan0 to egress ifb0
+# Redirect  ingress wlan0 to egress ifb0(並將wlan0的進入流量重新導向到ifb0)
 sudo tc filter add dev $int1 parent ffff: protocol ip u32 match u32 0 0 action mirred egress redirect dev $tin1
-# Add classes per ip
+# Add classes per ip(新增根類別)
 sudo tc qdisc add dev $tin1 root handle 2: htb default 80
 
+#設置總數據速率
 sudo tc class add dev $tin1 parent 2: classid 2:1 htb rate $total_th$unit  # create parent class
 
 # distribute data rate to the host (child class)
+#為每個IP新增子類別，分配對應的數據速率
 w=1
 class_id=10
 while [ $w -le $totalDatarate ]
@@ -52,6 +58,7 @@ w=$(( w+1 ))
 class_id=$(( class_id+10 ))
 done
 
+#新增過濾器，將流量根據IP匹配到對應的類別
 w=1
 class_id=10
 while [ $w -le $totalDatarate ]
--- traffic_shaping_code/traffic_control_del.sh
+++ traffic_shaping_code/traffic_control_del.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #This scripts for delete the traffic shaping from the network.
 
+# 設定網路介面變數
 # Interface connect to out lan
 int1="wlan0"
 # Interface virtual for incomming traffic
 tin1="ifb0"
 
+#清除網路介面上的qdisc設定
 # Clean interface
 sudo tc qdisc del root dev $int1
 # Clean interface
